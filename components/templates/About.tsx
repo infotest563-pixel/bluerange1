f60d260e1061a782cb629b5dd7c1e8f7847c87ca
infotest563-pixel <infotest563@gmail.com>
2026-04-21 18:27:50 +0530

    ...

--- components/templates/About.tsx
+++ components/templates/About.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { getMedia } from '../../lib/wp';
+import DomainsContactForm from '../DomainsContactForm';
 
 export default async function About({ page }: { page: any }) {
     const acf = page.acf;
@@ -140,6 +141,20 @@ export default async function About({ page }: { page: any }) {
                                     <img src={featureImg} alt="image" />
                                 </div>
                             )}
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            {/* Get in Touch */}
+            <section className="all-sec-lastform sec-padd">
+                <div className="container">
+                    <div className="row all-lastform-inner tx-wht">
+                        <div className="bl-box col-md-12 all-lstform cu-formbx">
+                            <div className="wd-100 fade-in-top">
+                                <h2 className="mb-3">get in touch</h2>
+                                <DomainsContactForm />
+                            </div>
                         </div>
                     </div>
                 </div>
