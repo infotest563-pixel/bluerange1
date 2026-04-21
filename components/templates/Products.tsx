7e8dff8c8fdea271d436b598f2865eb5f707fc21
infotest563-pixel <infotest563@gmail.com>
2026-04-21 18:06:46 +0530

    ...

--- components/templates/Products.tsx
+++ components/templates/Products.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { getMedia, renderShortcode } from '../../lib/wp';
+import DomainsContactForm from '../DomainsContactForm';
 
 export default async function Products({ page }: { page: any }) {
     const acf = page.acf;
@@ -121,6 +122,20 @@ export default async function Products({ page }: { page: any }) {
                                     <div dangerouslySetInnerHTML={{ __html: helpQSShortcodeHtml }} suppressHydrationWarning />
                                 </div>
                             )}
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            {/* Get in Touch */}
+            <section className="all-sec-lastform sec-padd">
+                <div className="container">
+                    <div className="row all-lastform-inner tx-wht">
+                        <div className="bl-box col-md-12 all-lstform cu-formbx">
+                            <div className="wd-100 fade-in-top">
+                                <h2 className="mb-3">get in touch</h2>
+                                <DomainsContactForm />
+                            </div>
                         </div>
                     </div>
                 </div>
