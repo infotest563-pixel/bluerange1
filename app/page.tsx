5dc8ae1ffba4a57ce6c3b4acb103dba042a741fb
infotest563-pixel <infotest563@gmail.com>
2026-04-28 18:26:23 +0530

    .

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getSettings, getPageById } from '../lib/wp';
-import { redirect } from 'next/navigation';
-
-export default async function Home() {
-  // Redirect root to /sv (Swedish is default)
-  redirect('/sv');
-}
